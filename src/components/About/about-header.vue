caa41515ec1df281900fac3589535be6f58beeb4
dagbuelawrence <dagbuel@gmail.com>
2023-09-16 20:30:18 +0100

    (third push)

--- src/components/About/about-header.vue
+++ src/components/About/about-header.vue
@@ -15,7 +15,7 @@
         </div>
       </sequential-entrance>
       <div class="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/pYp3bu-udYk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QiI6zYZ6rTs?si=zGf19mhJhlJBx5Y4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
   </div>
